618c7d985cea16c6c0e357c56c3a2e138fd2b093
Akshat Khurana <akshat.khurana@yahoo.com>
2024-07-22 21:12:59 +1000

    Built a selection menu
    Got the switch between the main and selection screens working - now I have to make the buttons to choose the search algorithm actually do something.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+{   
+    public static GameManager Instance;
+    public static string searchMode = "BFS";
+    private void Awake() {
+        if (Instance == null) Instance = this;
     }
+    public UnityEvent onStart = new UnityEvent();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void StartVisualiser() {
+        onStart.Invoke();
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject startScreen;
+    [SerializeField] private GameObject selectionScreen;
+    GameManager gm;
     void Start()
     {
-        
+        gm = GameManager.Instance;
+        gm.onStart.AddListener(DeactivateStartMenu);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void DeactivateStartMenu() {
+        startScreen.SetActive(false);
+        selectionScreen.SetActive(true);
+    }
+
+    public void StartButtonHandler() {
+        gm.StartVisualiser();
     }
 }
