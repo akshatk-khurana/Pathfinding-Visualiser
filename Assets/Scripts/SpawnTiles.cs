25b74504119888428e7ccaebf5198381e9488840
Akshat Khurana <akshat.khurana@yahoo.com>
2024-07-23 13:00:20 +1000

    Half-working grid generation system
    I really need to have grid generation  in place for my pathfinder to actually allow users to select squares. Right now the squares are very spaced out - which is something I don't want. Instead, they should be side by side.

--- Assets/Scripts/SpawnTiles.cs
+++ Assets/Scripts/SpawnTiles.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 public class SpawnTiles : MonoBehaviour
 {
     [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+    [SerializeField] private Transform camera;
+
     void Start()
     {
         GameManager.Instance.onStart.AddListener(SpawnMapTiles);
     }
 
     private void SpawnMapTiles() {
-        int row = 10;
-        int col = 20;
-        
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
             }
         }
     }
